2cc88f5dd904013d0a87b9293d16cb8edea06597
jkillz2020 <kilgore.josh@gmail.com>
2016-12-14 13:47:47 -0600

    api call function working on user search

--- javascripts/controllers/PlacesApiCtrl.js
+++ javascripts/controllers/PlacesApiCtrl.js
@@ -2,8 +2,9 @@
 
 app.controller("PlacesApiCtrl", function($scope, $rootScope, $routeParams, PlacesFactory){
   let searchInfo = $scope.input
-  $scope.getPlaces = function(){
+  $scope.getPlaces = function(searchInfo){
     PlacesFactory.getPlaces(searchInfo).then(function(response){
+      console.log("searchInfo passed into function", searchInfo);
     $scope.placeInfo = response.results; 
     console.log("placeInfo", $scope.placeInfo);
     })
